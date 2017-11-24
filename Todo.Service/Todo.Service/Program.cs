f46babba19ff9ca862c82db59c3dd2058e601f6d
Jim Shaw <jimxshaw@gmail.com>
2017-11-23 20:10:54 -0600

    Initial commit.

---
+++ Todo.Service/Todo.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceProcess;
+
+namespace Todo.Service
+{
+    static class Program
+    {
+        static void Main()
+        {
+            ServiceBase.Run(new Reminders());
+        }
+    }
+}
